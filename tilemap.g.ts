7298e64b844bc08dea168eda819636992e3203b4
Daphne Lai <daphnelai.q@gmail.com>
2022-11-04 14:28:51 +0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,11 +2,55 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "tilePlatform":
             case "tile1":return tile1;
+            case "tileA":
+            case "tile2":return tile2;
+            case "tileB":
+            case "tile3":return tile3;
+            case "tileC":
+            case "tile4":return tile4;
+            case "tileD":
+            case "tile5":return tile5;
+            case "tileE":
+            case "tile6":return tile6;
+            case "tileF":
+            case "tile7":return tile7;
+            case "tileG":
+            case "tile8":return tile8;
+            case "tileAb":
+            case "tile9":return tile9;
+            case "tileBb":
+            case "tile10":return tile10;
+            case "tileEb":
+            case "tile11":return tile11;
+            case "tileLadder":
+            case "tile12":return tile12;
         }
         return null;
     })
