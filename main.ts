aecf8ed70b3cb7a018acb6f49b5b6176a3baaab6
Daphne Lai <daphnelai.q@gmail.com>
2022-11-04 15:10:25 +0800

    Updating

--- main.ts
+++ main.ts
@@ -1,10 +1,34 @@
+// CUT SCENE
+// CONDUCTOR
 function initPlayer () {
     conductor = sprites.create(assets.image`imgConductor01`, SpriteKind.Player)
     scene.cameraFollowSprite(conductor)
     controller.moveSprite(conductor)
     conductor.setPosition(80, 180)
+    conductor.ay = 300
 }
+// SET UP LEVEL
+function gameStatus () {
+    tiles.setCurrentTilemap(mapLevel[currentLevel])
+}
+info.onLifeZero(function () {
+    game.over(false, effects.confetti)
+})
 let conductor: Sprite = null
-tiles.setCurrentTilemap(tilemap`level1`)
+let mapLevel: tiles.TileMapData[] = []
+let currentLevel = 0
+let song1 = [0]
+let song2 = [0]
+let song3 = [0]
+let songList = [song1, song2, song3]
+let correctNotes = songList[currentLevel]
+mapLevel = [tilemap`level1`, tilemap`level2`, tilemap`leve3`]
+info.setLife(3)
+
+// SET UP
+tiles.setCurrentTilemap(mapLevel[currentLevel])
 scene.setBackgroundImage(assets.image`imgConcertHall`)
 initPlayer()
+game.onUpdate(function () {
+	
+})
