02ec93317a5c6b265444d2315cd2d8af586d53f2
Daphne Lai <daphnelai.q@gmail.com>
2022-11-04 15:36:55 +0800

    Updated with music sheets

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const Sheet = SpriteKind.create()
+}
 // CUT SCENE
 // CONDUCTOR
 function initPlayer () {
@@ -14,20 +17,38 @@ function gameStatus () {
 info.onLifeZero(function () {
     game.over(false, effects.confetti)
 })
+// currentSheet.setPosition(0, 0)
+function initSong () {
+    song1 = [0]
+    song2 = [0]
+    song3 = [0]
+    musicSheet = [musicSheet1, musicSheet2, musicSheet3]
+    songList = [song1, song2, song3]
+    correctNotes = songList[currentLevel]
+    musicSheet1 = sprites.create(assets.image`imgMusic1`, SpriteKind.Sheet)
+    musicSheet2 = sprites.create(assets.image`imgMusic2`, SpriteKind.Sheet)
+    musicSheet3 = sprites.create(assets.image`imgMusic3`, SpriteKind.Sheet)
+    currentSheet = musicSheet[currentLevel]
+}
+let currentSheet: Sprite = null
+let correctNotes: number[] = []
+let songList: number[][] = []
+let musicSheet3: Sprite = null
+let musicSheet2: Sprite = null
+let musicSheet1: Sprite = null
+let musicSheet: Sprite[] = []
+let song3: number[] = []
+let song2: number[] = []
+let song1: number[] = []
 let conductor: Sprite = null
-let mapLevel: tiles.TileMapData[] = []
 let currentLevel = 0
-let song1 = [0]
-let song2 = [0]
-let song3 = [0]
-let songList = [song1, song2, song3]
-let correctNotes = songList[currentLevel]
-mapLevel = [tilemap`level1`, tilemap`level2`, tilemap`leve3`]
+let mapLevel: tiles.TileMapData[] = []
+mapLevel = [tilemap`level1`, tilemap`level2`, tilemap`level10`]
 info.setLife(3)
-
 // SET UP
 tiles.setCurrentTilemap(mapLevel[currentLevel])
 scene.setBackgroundImage(assets.image`imgConcertHall`)
+initSong()
 initPlayer()
 game.onUpdate(function () {
 	
